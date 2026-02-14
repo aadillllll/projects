fdc519e2dcb18ee06c09cf12ab2916db178f2262
aadillllll <adilmuhammeda8@gmail.com>
2026-02-14 14:13:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("contactBtn").addEventListener("click", function () {
+        alert("Thanks for visiting my portfolio! Feel free to connect with me.");
+    });
+});
